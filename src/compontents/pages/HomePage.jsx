d5ab302bdda112215a1f190008e5eae9705c0ac2
YuriKhedoyan <97390659+YuriKhedoyan@users.noreply.github.com>
2023-03-23 20:27:51 +0400

    Add HomePage.jsx

---
+++ src/compontents/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Container, TextField, Button } from "@mui/material";
+
+import Header from "../common/Header";
+import '../../Assets/Styles/HomePage.sass'
+
+const RegisterPage = () => {
+  return (
+    <>
+      <Container>
+        <Header></Header>
+      </Container>
+      <section id="sc1">
+      </section>
+    </>
+  )
+}
+
+export default RegisterPage;
