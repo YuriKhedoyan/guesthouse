6c43779149ef33ed208a8295fdb388664bb7024a
YuriKhedoyan <97390659+YuriKhedoyan@users.noreply.github.com>
2023-03-26 22:20:09 +0400

    Change HomePage.jsx

--- src/compontents/pages/HomePage.jsx
+++ src/compontents/pages/HomePage.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { Container, TextField, Button } from "@mui/material";
 
 import Header from "../common/Header";
+
 import '../../Assets/Styles/HomePage.sass'
 
 const RegisterPage = () => {
   return (
     <>
       <Container>
-        <Header></Header>
+        <Header/>
       </Container>
-      <section id="sc1">
+      <section>
       </section>
     </>
   )
