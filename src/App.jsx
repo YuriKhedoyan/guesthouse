09a50be650a67628af82b00425adf7184502662a
YuriKhedoyan <97390659+YuriKhedoyan@users.noreply.github.com>
2023-05-11 13:55:15 +0400

    Fix error hmr updating

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,14 @@ import { withSuspense } from "./helpers/withSuspense";
 
 import ThemeContext from "./locales/themeContext";
 
-const Home = withSuspense(lazy(() => import("./pages/Home")));
-const Rooms = withSuspense(lazy(() => import("./pages/Rooms")));
-const Gallery = withSuspense(lazy(() => import("./pages/Gallery")));
-const ContactUs = withSuspense(lazy(() => import("./pages/ContactUs")));
-const Login = withSuspense(lazy(() => import("./pages/register/Login")));
-const Restaurant = withSuspense(lazy(() => import("./pages/Restaurant")));
-const Register = withSuspense(lazy(() => import("./pages/register/Register")));
+const Home = withSuspense(lazy(() => import("./pages/home")));
+const Rooms = withSuspense(lazy(() => import("./pages/rooms")));
+const Gallery = withSuspense(lazy(() => import("./pages/gallery")));
+const ContactUs = withSuspense(lazy(() => import("./pages/contactUs")));
+const Restaurant = withSuspense(lazy(() => import("./pages/restaurant")));
 
 const App = () => {
 	const [theme, setTheme] = useState("light");
-	console.log("и");
 
 	return (
 		<ThemeContext.Provider value={{theme, setTheme}}>
@@ -23,9 +20,7 @@ const App = () => {
 				<Routes>
 					<Route path="/" element={<Home />}/>
 					<Route path="/rooms" element={<Rooms />} />
-					<Route path="/login" element={<Login />} />
 					<Route path="/gallery" element={<Gallery />} />
-					<Route path="/register" element={<Register />} />
 					<Route path="/contactUs" element={<ContactUs />} />
 					<Route path="/restaurant" element={<Restaurant />} />
 				</Routes>
