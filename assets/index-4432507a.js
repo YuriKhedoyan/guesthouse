9da192ff62ce126ad55f9266e36c9d15cd92b718
Arsen Aleksanyan <arsen.aleksanyan.1994@gmail.com>
2023-06-25 13:53:54 +0400

    Updates

---
+++ assets/index-4432507a.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-2bcf4428.js";const s=m=>o("img",{...m}),a=s;export{a as default};
--- assets/index-e2d795d4.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./index-78456f21.js";const s=m=>o("img",{...m}),a=s;export{a as default};
