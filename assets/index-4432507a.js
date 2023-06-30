3ed421005fa5b2d0c731f8c44fb6578ca2709209
YuriKhedoyan <97390659+YuriKhedoyan@users.noreply.github.com>
2023-06-30 14:35:44 +0400

    Updates

--- assets/index-4432507a.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./index-2bcf4428.js";const s=m=>o("img",{...m}),a=s;export{a as default};
---
+++ assets/index-e2d795d4.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-78456f21.js";const s=m=>o("img",{...m}),a=s;export{a as default};
