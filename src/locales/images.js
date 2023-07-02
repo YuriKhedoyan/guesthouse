451aae8a0b64f76f5cb15b2304db5f034ed5bdf5
YuriKhedoyan <97390659+YuriKhedoyan@users.noreply.github.com>
2023-07-02 23:34:18 +0400

    Change imges src

--- src/locales/images.js
+++ src/locales/images.js
@@ -1,136 +1,136 @@
 const data = {
 	house: [
 		{
-			src: "/src/public/images/house.jpg",
+			src: "../../public/images/house.jpg",
 			alt: "Photo of the House",
 		},
 		{
-			src: "/src/public/images/house1.jpg",
+			src: "../../public/images/house1.jpg",
 			alt: "Photo of the House",
 		},
 		{
-			src: "/src/public/images/house2.jpg",
+			src: "../../public/images/house2.jpg",
 			alt: "Photo of the House",
 		},
 		{
-			src: "/src/public/images/house3.jpg",
+			src: "../../public/images/house3.jpg",
 			alt: "Photo of the House",
 		},
 		{
-			src: "/src/public/images/house4.jpg",
+			src: "../../public/images/house4.jpg",
 			alt: "Photo of the House",
 		},
 		{
-			src: "/src/public/images/houseRestaurant.jpg",
+			src: "../../public/images/houseRestaurant.jpg",
 			alt: "Photo of the House's Restaurant",
 		},
 	],
 	restaurant: [
 		{
-			src: "/src/public/images/restaurant1.jpg",
+			src: "../../public/images/restaurant1.jpg",
 			alt: "Photo of the Restaurant",
 		},
 		{
-			src: "/src/public/images/restaurant2.jpg",
+			src: "../../public/images/restaurant2.jpg",
 			alt: "Photo of the Restaurant",
 		},
 		{
-			src: "/src/public/images/restaurant3.jpg",
+			src: "../../public/images/restaurant3.jpg",
 			alt: "Photo of the Restaurant",
 		},
 		{
-			src: "/src/public/images/restaurant4.jpg",
+			src: "../../public/images/restaurant4.jpg",
 			alt: "Photo of the Restaurant",
 		},
 		{
-			src: "/src/public/images/restaurant5.jpg",
+			src: "../../public/images/restaurant5.jpg",
 			alt: "Photo of the Restaurant",
 		},
 		{
-			src: "/src/public/images/restaurant6.jpg",
+			src: "../../public/images/restaurant6.jpg",
 			alt: "Photo of the Restaurant",
 		},
 		{
-			src: "/src/public/images/restaurant7.jpg",
+			src: "../../public/images/restaurant7.jpg",
 			alt: "Photo of the Restaurant",
 		},
 	],
 	garden: [
 		{
-			src: "/src/public/images/garden1.jpg",
+			src: "../../public/images/garden1.jpg",
 			alt: "Photo of the Garden",
 		},
 		{
-			src: "/src/public/images/garden2.jpg",
+			src: "../../public/images/garden2.jpg",
 			alt: "Photo of the Garden",
 		},
 		{
-			src: "/src/public/images/garden3.jpg",
+			src: "../../public/images/garden3.jpg",
 			alt: "Photo of the Garden",
 		},
 		{
-			src: "/src/public/images/garden4.jpg",
+			src: "../../public/images/garden4.jpg",
 			alt: "Photo of the Garden",
 		},
 		{
-			src: "/src/public/images/garden5.jpg",
+			src: "../../public/images/garden5.jpg",
 			alt: "Photo of the Garden",
 		},
 		{
-			src: "/src/public/images/garden6.jpg",
-			alt: "Photo of the Garden",
+			src: "../../public/images/garden6.jpg",
+			alt: "../../Photo of the Garden",
 		},
 		{
-			src: "/src/public/images/garden7.jpg",
-			alt: "Photo of the Garden",
+			src: "public/images/garden7.jpg",
+			alt: "../../Photo of the Garden",
 		},
 		{
-			src: "/src/public/images/garden8.jpg",
-			alt: "Photo of the Garden",
+			src: "public/images/garden8.jpg",
+			alt: "../../Photo of the Garden",
 		},
 		{
-			src: "/src/public/images/garden9.jpg",
-			alt: "Photo of the Garden",
+			src: "public/images/garden9.jpg",
+			alt: "../../Photo of the Garden",
 		},
 		{
-			src: "/src/public/images/garden10.jpg",
-			alt: "Photo of the Garden",
+			src: "public/images/garden10.jpg",
+			alt: "../../Photo of the Garden",
 		},
 	],
 	rooms: {
 		standardRoom: {
-			src: "/src/public/images/restaurant1.jpg",
+			src: "../../public/images/restaurant1.jpg",
 			alt: "Photo of the Standard Room",
 		},
 		deluxe: {
-			src: "/src/public/images/restaurant2.jpg",
+			src: "../../public/images/restaurant2.jpg",
 			alt: "Photo of the Deluxe Room",
 		},
 		deluxeSuperior: {
-			src: "/src/public/images/restaurant3.jpg",
+			src: "../../public/images/restaurant3.jpg",
 			alt: "Photo of the Deluxe Superior",
 		},
 		duplex: {
-			src: "/src/public/images/restaurant4.jpg",
+			src: "../../public/images/restaurant4.jpg",
 			alt: "Photo of the Duplex Room",
 		},
 	},
 	otherPictures: {
 		spruceBranch: {
-			src: "/src/public/images/9S5A9235-1-scaled.jpg",
+			src: "../../public/images/9S5A9235-1-scaled.jpg",
 			alt: "Spruce Branch",
 		},
 		languages: {
 			am: {
-				src: "/src/public/images/flag-of-armenia-logo.png",
+				src: "../../public/images/flag-of-armenia-logo.png",
 				alt: "Armenian Flag"
 			},
 			ru: {
-				src: "/src/public/images/flag-of-russia-logo.png",
+				src: "../../public/images/flag-of-russia-logo.png",
 				alt: "Russian Flag"
 			},
 			en: {
-				src: "/src/public/images/flag-of-america-logo.png",
+				src: "../../public/images/flag-of-america-logo.png",
 				alt: "American Flag"
 			}
 		}
