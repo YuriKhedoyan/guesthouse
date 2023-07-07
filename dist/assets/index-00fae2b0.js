a9243ffdc051f45a92da040962071807cd9179ea
YuriKhedoyan <97390659+YuriKhedoyan@users.noreply.github.com>
2023-07-07 11:18:17 +0400

    Fix stlyes in footer

--- dist/assets/index-00fae2b0.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./index-ee76118f.js";const s=m=>o("img",{...m}),a=s;export{a as default};
---
+++ dist/assets/index-e99ceed3.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-46bdb99d.js";const s=m=>o("img",{...m}),a=s;export{a as default};
