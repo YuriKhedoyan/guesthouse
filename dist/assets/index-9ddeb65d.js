ec34142382e592e576a3532994616410809d0730
YuriKhedoyan <97390659+YuriKhedoyan@users.noreply.github.com>
2023-07-09 20:57:10 +0400

    Fix styles in home page

--- dist/assets/index-9ddeb65d.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./index-a77784da.js";const s=m=>o("img",{...m}),a=s;export{a as default};
---
+++ dist/assets/index-f7fd5355.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-5dbbc5c6.js";const s=m=>o("img",{...m}),a=s;export{a as default};
