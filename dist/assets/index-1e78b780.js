ab089f49f2786ef7f5c5794271daa5cadfd4f434
YuriKhedoyan <97390659+YuriKhedoyan@users.noreply.github.com>
2023-07-12 22:38:24 +0400

    Fix styles

--- dist/assets/index-1e78b780.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./index-8ad1a42b.js";const s=m=>o("img",{...m}),a=s;export{a as default};
---
+++ dist/assets/index-a72da3f6.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-539df6a5.js";const s=m=>o("img",{...m}),a=s;export{a as default};
